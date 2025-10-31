8ce057059d449dd371c7ca27eb75b7a8a904907a
kIwwwww1 <danil29070@gmail.com>
2025-10-31 19:50:44 +0300

    Промежуточный коммит

--- main.py
+++ main.py
@@ -18,10 +18,10 @@ base_url = f'https://data.fixer.io/api/latest?access_key={TOKEN}'
 
 class CreateUser:
 
-    def __init__(self) -> None:
-        self.user_id = None
-        self.name = None
-        self.user_value = None
+    # def __init__(self) -> None:
+    #     self.user_id = None
+    #     self.name = None
+    #     self.user_value = None
 
     def create_user(self):
         user_in_redis = redis_client.get(name='user:1') # Здесь написано постоянное значение "user:1" (что нельзя делать), 
@@ -31,44 +31,69 @@ class CreateUser:
             return f'У вас уже есть пользователь {user_in_redis}'
         user_id = len(data) + 1
         user_name = input('Введите имя пользователя "Кирилл": ')
-        user_value = input('Укажите свою валюту "РУБ", "EUR"", "USD": ').upper()
-        if user_value not in base_value:
-            return f'Вы ввели не верные данные, пожалуйста начните заново'
-        user_data = {
-            'id': user_id,
-            'name': user_name,
-            'user_value': user_value,
-        }
-        redis_client.setex(name=f'user:{user_id}' ,time=25 ,value=dumps(user_data))
-        data.append(user_data)
-        return 'Данные добавлены'
-    
+        try:
+            user_value = input(f'Укажите свою валюту {base_value}: ').upper()
+            if user_value not in base_value:
+                print(f'Вы ввели не верные данные, пожалуйста начните заново')
+            base_value.remove(user_value)
+            user_buy = input(f'Укажите покупаемую валюту {base_value}: ').upper()
+            if user_buy not in base_value:
+                print(f'Вы ввели не верные данные, пожалуйста начните заново')
+            user_data = {
+                'id': user_id,
+                'name': user_name,
+                'user_value': user_value,
+                'user_buy': user_buy,
+            }
+            redis_client.set(name=f'user:{user_id}' ,value=dumps(user_data))
+            data.append(user_data)
+            print('Данные добавлены')
+        except Exception:
+            print('Произошла ошибка')
 
-async def main_func(self):
-    async with httpx.AsyncClient() as client:
-        print('Получаем данные...')
-        r = await client.get(base_url)
-        print('Получили данные')
-        return loads(r.content)
+    def buy_value(self):
+        user_data = loads(redis_client.get(name='user:1'))
+        print(user_data)
+
+        count_for_buy = int(input(f'Какое количество {user_data} купить?'))
+        r = httpx.get(base_url)
+        print(r.content)
+
+
+# async def buy_value():
+#     async with httpx.AsyncClient() as client:
+#         r = await client.get(base_url)
+#         print('Получили данные')
+#         return loads(r.content)
         
 async def main():
 
-    # 1 Создать пользвателя
-    # 2 Указать валюту пользователя
-    # 3 Все пользователи
-
     while True:
+        print('==========================')
+        print('''1 | Создать пользователя\n2 | Купить валюту\n3 | Все пользователи\n0 | Выйти''')
+        print('==========================')
+
         user_map =  int(input('Выберите пункт: '))
+        print('==========================')
 
         match user_map:
             case 1:
                 print(CreateUser().create_user())
                 
             case 2:
-                pass
+                print(CreateUser().buy_value())
                 
             case 3:
-                print(data)
+                user_in_redis = redis_client.get('user:1')
+                if user_in_redis:
+                    print(loads(user_in_redis))
+                else:
+                    print(data) if len(data) >= 1 else print('В базе пусто')
+            case 0:
+                print('Вы вышли из программы')
+                break
+            case _:
+                print('Я тебя не понимаю')
 
 
 
